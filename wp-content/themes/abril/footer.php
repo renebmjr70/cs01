517d95046082532e64b9e659c8fbd2b0bf645458
renenutra <caixao@renebmjr.com.br>
2017-11-21 14:36:36 -0200

    Ed. Abril - Teste 02 - 4 Rodas

---
+++ wp-content/themes/abril/footer.php
@@ -0,0 +1,63 @@
+        <footer class="footer" id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="wrapper-referrers">
+                            <div class="menu-sites-abril-container">
+                                <ul id="menu-sites-abril" class="referrers">
+                                    <li id="menu-item-151015" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-151015"><a target="_blank" href="http://arquiteturaeconstrucao.abril.com.br/">Arquitetura &amp; Construção</a></li>
+                                    <li id="menu-item-150677" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150677"><a target="_blank" href="http://bebe.abril.com.br">Bebê.com</a></li>
+                                    <li id="menu-item-150678" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150678"><a target="_blank" href="http://boaforma.abril.com.br">Boa Forma</a></li>
+                                    <li id="menu-item-150715" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150715"><a target="_blank" href="http://capricho.abril.com.br">Capricho</a></li>
+                                    <li id="menu-item-150680" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150680"><a target="_blank" href="http://casa.abril.com.br">CASA.com</a></li>
+                                    <li id="menu-item-150682" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150682"><a target="_blank" href="http://casaclaudia.abril.com.br">CASA Claudia</a></li>
+                                    <li id="menu-item-150687" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150687"><a target="_blank" href="http://casacor.abril.com.br">CASACOR</a></li>
+                                    <li id="menu-item-150689" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150689"><a target="_blank" href="http://claudia.abril.com.br">Claudia</a></li>
+                                    <li id="menu-item-150695" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150695"><a target="_blank" href="http://cosmopolitan.abril.com.br">Cosmopolitan</a></li>
+                                    <li id="menu-item-150696" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150696"><a target="_blank" href="http://elle.abril.com.br">Elle</a></li>
+                                    <li id="menu-item-150697" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150697"><a target="_blank" href="http://estilo.abril.com.br">Estilo</a></li>
+                                    <li id="menu-item-150698" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150698"><a target="_blank" href="http://exame.abril.com.br">EXAME.com</a></li>
+                                    <li id="menu-item-150699" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150699"><a target="_blank" href="http://guiadoestudante.abril.com.br">Guia do Estudante</a></li>
+                                    <li id="menu-item-150700" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150700"><a target="_blank" href="http://mdemulher.abril.com.br">MdeMulher</a></li>
+                                    <li id="menu-item-151016" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-151016"><a target="_blank" href="http://minhacasa.abril.com.br/">Minha Casa</a></li>
+                                    <li id="menu-item-150701" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150701"><a target="_blank" href="http://mundoestranho.abril.com.br">Mundo Estranho</a></li>
+                                    <li id="menu-item-150702" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150702"><a target="_blank" href="http://veja.abril.com.br/placar/">Placar</a></li>
+                                    <li id="menu-item-150703" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150703"><a target="_blank" href="http://saude.abril.com.br">Saúde</a></li>
+                                    <li id="menu-item-150704" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150704"><a target="_blank" href="http://super.abril.com.br">Superinteressante</a></li>
+                                    <li id="menu-item-150705" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150705"><a target="_blank" href="http://veja.abril.com.br">VEJA.com</a></li>
+                                    <li id="menu-item-150706" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150706"><a target="_blank" href="http://vejario.abril.com.br">VEJA Rio</a></li>
+                                    <li id="menu-item-150707" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150707"><a target="_blank" href="http://vejasp.abril.com.br">VEJA São Paulo</a></li>
+                                    <li id="menu-item-150711" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150711"><a target="_blank" href="http://viagemeturismo.abril.com.br">Viagem e Turismo</a></li>
+                                    <li id="menu-item-150712" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150712"><a target="_blank" href="http://vip.abril.com.br">VIP</a></li>
+                                </ul>
+                            </div>
+                            <ul class="custom-columns">
+                                <li class="">
+                                    <!-- <a href="/fale-conosco">Fale conosco</a>
+                                        <a href="/termos-de-uso">Termos de Uso</a>
+                                        <a href="#">FAQ</a> -->
+                                </li>
+                                <li id="menu-item-150714" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-150714"><a href="https://quatrorodas.abril.com.br/como-desativar-adblock/">Como desativar o AdBlock</a></li>
+                                <li id="menu-item-150713" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-150713"><a href="https://quatrorodas.abril.com.br/newsletter/">Newsletter</a></li>
+                            </ul>
+                            <ul class="social-columns">
+                                <li>
+                                    <span>SIGA</span>
+                                </li>
+                                <li id="menu-item-77598" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-77598"><a href="https://www.facebook.com/quatrorodas"><i class=""></i>Facebook</a></li>
+                                <li id="menu-item-77601" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-77601"><a href="https://www.instagram.com/quatro_rodas/"><i class=""></i>Instagram</a></li>
+                                <li id="menu-item-77599" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-77599"><a href="https://twitter.com/quatrorodas"><i class=""></i>Twitter</a></li>
+                                <li id="menu-item-77600" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-77600"><a href="https://plus.google.com/115638687340706894168"><i class=""></i>Google Plus</a></li>
+                                <li id="menu-item-77602" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-77602"><a href="https://quatrorodas.abril.com.br/rss/"><i class=""></i>RSS</a></li>
+                            </ul>
+                        </div>
+                        <div class="abril-logo">
+                            <a href="http://www.abril.com.br" target="_blank"><img src="https://s0.wp.com/wp-content/themes/vip/abril-master/inc/images/logo-grupo-abril.png"></a>
+                        </div>
+                        <div class="copyright">Copyright © Abril Mídia S A. Todos os direitos reservados.</div>
+                        <div class="privacy"><a href="/politica-de-privacidade">Política de Privacidade</a></div>
+                        <div class="wordpress">Powered by <a href="https://vip.wordpress.com/?utm_source=vip_powered_wpcom&amp;utm_medium=web&amp;utm_campaign=VIP%20Footer%20Credit&amp;utm_term=quatrorodas.abril.com.br" rel="generator nofollow" class="powered-by-wpcom">WordPress.com VIP</a></div>
+                    </div>
+                </div>
+            </div>
+        </footer>
